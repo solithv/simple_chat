ac17e6956a998ec3d539cfd60c229ad89fd5953a
solithv <120559288+solithv@users.noreply.github.com>
2024-11-19 18:47:13 +0900

    feature: render template client for test

--- server/libs/routes/http.py
+++ server/libs/routes/http.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from flask import Blueprint, jsonify, send_file
+from flask import Blueprint, abort, jsonify, render_template, send_file
 from libs.storage import transact
 
 http_module = Blueprint("http_routes", __name__)
@@ -11,16 +11,18 @@ def main():
     return jsonify({"status": "http online"})
 
 
-@http_module.get("/files/<int:id>")
+@http_module.route("/test")
+def test_client():
+    return render_template("index.html")
+
+
+@http_module.get("/files/<int:id>/<filename>")
 @transact
-def get_file(conn: sqlite3.Connection, id: int):
+def get_file(conn: sqlite3.Connection, id: int, filename: str):
     """ファイルダウンロード"""
     file = conn.execute("SELECT * FROM files WHERE id = ?", (id,)).fetchone()
+    if file is None or filename != file["filename"]:
+        abort(404)
     file_path = file["save_name"]
 
-    return send_file(file_path, download_name=file.file)
-
-
-def generate_link(file_path: str):
-    # TODO: download link generate
-    file_path
+    return send_file(file_path, download_name=file["filename"])
