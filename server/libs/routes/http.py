0d7154afed77d30f96449be92436c86c0b21bae1
solithv <120559288+solithv@users.noreply.github.com>
2024-12-09 12:19:45 +0900

    Revert "feature: server prefix"
    This reverts commit c72eb957490a30e567dbf48567e2a21496ef16ca.

--- server/libs/routes/http.py
+++ server/libs/routes/http.py
@@ -3,22 +3,20 @@ import sqlite3
 from flask import Blueprint, abort, jsonify, render_template, send_file
 from libs.storage import transact
 
-from libs.config import PREFIX
-
 http_module = Blueprint("http_routes", __name__)
 
 
-@http_module.route(f"{PREFIX}/")
+@http_module.route("/")
 def main():
     return jsonify({"status": "http online"})
 
 
-@http_module.route(f"{PREFIX}/test")
+@http_module.route("/test")
 def test_client():
     return render_template("index.html")
 
 
-@http_module.get(f"{PREFIX}/files/<int:id>/<filename>")
+@http_module.get("/files/<int:id>/<filename>")
 @transact
 def get_file(conn: sqlite3.Connection, id: int, filename: str):
     """ファイルダウンロード"""
